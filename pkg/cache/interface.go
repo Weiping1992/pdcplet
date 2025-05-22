74713d6a004f91568e2f94a7126693207a173a79
Weiping1992 <chengwei2011ww@163.com>
2025-05-22 09:33:50 +0800

    修改返回值名称

--- pkg/cache/interface.go
+++ pkg/cache/interface.go
@@ -1,7 +1,7 @@
 package cache
 
 type Cache interface {
-	Update(vmiName string, status VmiStatus) (bool, bool)
+	Update(vmiName string, status VmiStatus) (isStatusChanged bool, isReady bool)
 	MarkDelete(vmiName string)
 	DeleteDone(vmiName string)
 	SetTaskId(vmiName string, taskId int) error
